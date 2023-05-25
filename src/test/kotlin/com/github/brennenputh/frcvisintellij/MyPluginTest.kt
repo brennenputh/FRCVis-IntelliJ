4afb2c3694563966ec068a5a8cd19b1a38cae778
Brennen Puth <brennenputh@gmail.com>
2023-05-24 22:56:50 -0400

    Delete uneeded stuff

--- src/test/kotlin/com/github/brennenputh/frcvisintellij/MyPluginTest.kt
+++ src/test/kotlin/com/github/brennenputh/frcvisintellij/MyPluginTest.kt
@@ -1,12 +1,10 @@
 package com.github.brennenputh.frcvisintellij
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.brennenputh.frcvisintellij.services.GraphProviderService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -30,9 +28,6 @@ class MyPluginTest : BasePlatformTestCase() {
     }
 
     fun testProjectService() {
-        val projectService = project.service<GraphProviderService>()
-
-        assertNotSame(projectService.getRandomNumber(), projectService.getRandomNumber())
     }
 
     override fun getTestDataPath() = "src/test/testData/rename"
